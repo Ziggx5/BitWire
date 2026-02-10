92c9ccaf494754d74fb8058809edfedaa470b34b
Ziggx5 <maj.sedonja5@gmail.com>
2026-02-10 21:04:53 +0100

    Better UI, some small code fixes

--- client/client_modules/ui.py
+++ client/client_modules/ui.py
@@ -5,6 +5,7 @@ import socket
 import threading
 from client_modules.add_server import AddServer
 from client_modules.load_servers import server_loader
+
 class MainUi(QWidget):
     def __init__(self):
         super().__init__()
@@ -17,25 +18,35 @@ class MainUi(QWidget):
         self.setWindowTitle("BitWire")
         self.setStyleSheet("background-color : #0e1117;")
 
-        self.left_frame = QFrame(self)
-        self.left_frame.setGeometry(0, 100, 200, 500)
-        self.left_frame.setStyleSheet("background: transparent; border: 1px solid #737373")
+        self.server_frame = QFrame(self)
+        self.server_frame.setGeometry(0, 100, 200, 450)
+        self.server_frame.setStyleSheet("background: transparent; border: 1px solid #737373")
 
-        self.left_layout = QVBoxLayout(self.left_frame)
-        self.left_layout.setAlignment(Qt.AlignTop)
-        self.left_layout.setSpacing(1)
+        self.server_layout = QVBoxLayout(self.server_frame)
+        self.server_layout.setAlignment(Qt.AlignTop)
+        self.server_layout.setSpacing(1)
 
-        self.right_frame = QFrame(self)
-        self.right_frame.setGeometry(200, 0, 700, 600)
-        self.right_frame.setStyleSheet("background: transparent; border: 1px solid #737373")
+        self.main_frame = QFrame(self)
+        self.main_frame.setGeometry(200, 0, 700, 600)
+        self.main_frame.setStyleSheet("background: transparent; border: 1px solid #737373")
 
-        self.right_layout = QVBoxLayout(self.right_frame)
-        self.right_layout.setContentsMargins(10, 10, 10, 10)
-        self.right_layout.setSpacing(8)
+        self.main_layout = QVBoxLayout(self.main_frame)
+        self.main_layout.setContentsMargins(10, 10, 10, 10)
+        self.main_layout.setSpacing(8)
 
         self.upper_frame = QFrame(self)
         self.upper_frame.setGeometry(0, 50, 200, 50)
         self.upper_frame.setStyleSheet("background: transparent; border: 1px solid #737373")
+
+        self.user_frame = QFrame(self)
+        self.user_frame.setGeometry(0, 550, 200, 50)
+        self.user_frame.setStyleSheet("background: transparent; border: 1px solid #737373")
+
+        self.username_input = QTextEdit(self.user_frame)
+        self.username_input.setPlaceholderText("Name")
+        self.username_input.setFixedSize(100, 30)
+        self.username_input.setStyleSheet("border: 1px solid #737373")
+        self.username_input.move(10, 10)
 
         self.server_button_group = QButtonGroup(self)
         self.server_button_group.setExclusive(True)
@@ -84,8 +95,8 @@ class MainUi(QWidget):
         self.reload_servers()
 
     def reload_servers(self):
-        while self.left_layout.count():
-            item = self.left_layout.takeAt(0)
+        while self.server_layout.count():
+            item = self.server_layout.takeAt(0)
             widget = item.widget()
             if widget:
                 widget.deleteLater()
@@ -119,7 +130,7 @@ class MainUi(QWidget):
                     background-color: #262626;
                 }
             """)
-            self.left_layout.addWidget(server_button)
+            self.server_layout.addWidget(server_button)
             self.server_button_group.addButton(server_button)
             server_button.clicked.connect(self.load_chat)
     
@@ -128,8 +139,8 @@ class MainUi(QWidget):
             self.running = False
             self.client.close()
             self.client = None
-        while self.right_layout.count():
-            item = self.right_layout.takeAt(0)
+        while self.main_layout.count():
+            item = self.main_layout.takeAt(0)
             widget = item.widget()
             if widget:
                 widget.deleteLater()
@@ -147,18 +158,54 @@ class MainUi(QWidget):
         server_name = server_button.property("name")
         self.server_address = server_button.property("ip")
         self.running = True
-        self.receive_messages_thread = threading.Thread(target = self.receive_messages, args = (self.server_address,))
+        self.receive_messages_thread = threading.Thread(target = self.receive_messages, args = (self.server_address,), daemon = True)
         self.receive_messages_thread.start()
 
         self.chat_view = QTextBrowser()
-        self.chat_view.setStyleSheet("background-color: #1a1e24;")
+        self.chat_view.verticalScrollBar().setSingleStep(10)
+        self.chat_view.setStyleSheet("""
+            QTextBrowser {
+                background-color: #0d1117;
+                color: #e6edf3;
+                padding: 10px;
+                font-size: 14px;
+            }
+            QScrollBar:vertical {
+                background: transparent;
+                width: 15px;
+                border-radius: 6px;
+            }
+
+            QScrollBar:handle:vertical {
+                background-color: #333e4f;
+                border-radius: 6px;
+            }
+
+            QScrollBar:handle:vertical:hover {
+                background-color: #4d5d75;
+            }
+
+            QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical {
+                background-color: transparent;
+                border: transparent;
+            }
+        """)
+
         self.message_input = QTextEdit()
-        self.message_input.setFixedHeight(30)
+        self.message_input.setFixedHeight(40)
         self.message_input.setPlaceholderText("Type a message...")
         self.message_input.setStyleSheet("""
             QTextEdit {
                 border-radius: 10px;
                 background-color: #1a1e24;
+                color: #e6edf3;
+                padding: 5px 5px;
+                border: 1px solid #3b4657;
+            }
+
+            QTextEdit:focus {
+            border: 1px solid #505f75;
+            
             }
         """)
 
@@ -166,9 +213,9 @@ class MainUi(QWidget):
         send_message.setFixedSize(30, 30)
         send_message.clicked.connect(self.send_message)
 
-        self.right_layout.addWidget(self.chat_view)
-        self.right_layout.addWidget(self.message_input)
-        self.right_layout.addWidget(send_message)
+        self.main_layout.addWidget(self.chat_view)
+        self.main_layout.addWidget(self.message_input)
+        self.main_layout.addWidget(send_message)
 
     def receive_messages(self, ip_address):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
