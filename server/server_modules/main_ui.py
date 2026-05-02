e34ebf50fb6e9d5b1ef0b6f0cffe7b6353e7a881
Ziggx5 <maj.sedonja5@gmail.com>
2026-05-02 18:06:56 +0200

    feat: updated server app ui, added server folder button, removed browser buttons

--- server/server_modules/main_ui.py
+++ server/server_modules/main_ui.py
@@ -1,10 +1,11 @@
 from PySide6.QtWidgets import *
-from PySide6.QtCore import Qt, QTimer, Signal
-from PySide6.QtGui import QFont
+from PySide6.QtCore import *
+from PySide6.QtGui import *
 import threading
 from server_modules.data_manipulation import local_data_file, copy_to_data_dir, files_check
 from server_modules.server import ChatServer
 from server_modules.system_tray import TrayManager
+from server_modules.load_assets import file_root
 
 class MainUi(QWidget):
     def __init__(self):
@@ -17,6 +18,7 @@ class MainUi(QWidget):
         self.chat_server = ChatServer()
         self.chat_server.uptime_signal.connect(self.update_timer)
         local_data_file()
+        image_path = file_root()
         self.files = files_check()
 
         layout = QVBoxLayout(self)
@@ -26,8 +28,10 @@ class MainUi(QWidget):
         certificate_file_layout = QHBoxLayout()
         key_file_layout = QHBoxLayout()
 
-        database_box = QGroupBox("Database file")
-        database_file_layout = QHBoxLayout()
+        databases_box = QGroupBox("Database files")
+        database_files_layout = QVBoxLayout()
+        users_database_layout = QHBoxLayout()
+        messages_database_layout = QHBoxLayout()
 
         server_control_box = QGroupBox("Server Control")
         server_control_box_layout = QVBoxLayout()
@@ -35,20 +39,27 @@ class MainUi(QWidget):
         server_uptime_layout = QHBoxLayout()
         server_buttons_layout = QHBoxLayout()
 
+        server_folder_layout = QHBoxLayout()
+
         certificate_file_label = QLabel("Certificate file:")
         self.certificate_file_input = QLineEdit()
-        self.certificate_file_button = QPushButton("Browse...")
-        self.certificate_file_button.clicked.connect(lambda: self.send_file_path(".crt"))
+        #self.certificate_file_button = QPushButton("Browse...")
+        #self.certificate_file_button.clicked.connect(lambda: self.send_file_path("certificate"))
 
         key_file_label = QLabel("Key file:")
         self.key_file_input = QLineEdit()
-        self.key_file_button = QPushButton("Browse...")
-        self.key_file_button.clicked.connect(lambda: self.send_file_path(".key"))
+        #self.key_file_button = QPushButton("Browse...")
+        #self.key_file_button.clicked.connect(lambda: self.send_file_path("key"))
 
-        database_file_label = QLabel("Database file:")
-        self.database_file_input = QLineEdit()
-        self.database_file_button = QPushButton("Browse...")
-        self.database_file_button.clicked.connect(lambda: self.send_file_path(".db"))
+        users_database_file_label = QLabel("Users database file:")
+        self.users_database_file_input = QLineEdit()
+        #self.users_database_file_button = QPushButton("Browse...")
+        #self.users_database_file_button.clicked.connect(lambda: self.send_file_path("users_database"))
+
+        messages_database_file_label = QLabel("Messages database file:")
+        self.messages_database_file_input = QLineEdit()
+        #self.messages_database_file_button = QPushButton("Browse...")
+        #self.messages_database_file_button.clicked.connect(lambda: self.send_file_path("messages_database"))
 
         self.start_server_button = QPushButton("Start Server")
         self.start_server_button.clicked.connect(self.start_server)
@@ -62,24 +73,36 @@ class MainUi(QWidget):
         server_uptime_label = QLabel("Server Uptime")
         self.server_uptime_time = QLabel("Time")
 
+        server_folder_button = QPushButton()
+        server_folder_button.setIcon(QIcon(f"{image_path}/folder.png"))
+        server_folder_button.setIconSize(QSize(15, 15))
+        server_folder_button.setFixedSize(35, 35)
+
         certificate_file_layout.addWidget(certificate_file_label)
         certificate_file_layout.addWidget(self.certificate_file_input)
-        certificate_file_layout.addWidget(self.certificate_file_button)
+        #certificate_file_layout.addWidget(self.certificate_file_button)
 
         key_file_layout.addWidget(key_file_label)
         key_file_layout.addWidget(self.key_file_input)
-        key_file_layout.addWidget(self.key_file_button)
+        #key_file_layout.addWidget(self.key_file_button)
 
         ssl_box_layout.addLayout(certificate_file_layout)
         ssl_box_layout.addLayout(key_file_layout)
 
         ssl_box.setLayout(ssl_box_layout)
 
-        database_file_layout.addWidget(database_file_label)
-        database_file_layout.addWidget(self.database_file_input)
-        database_file_layout.addWidget(self.database_file_button)
+        users_database_layout.addWidget(users_database_file_label)
+        users_database_layout.addWidget(self.users_database_file_input)
+        #users_database_layout.addWidget(self.users_database_file_button)
 
-        database_box.setLayout(database_file_layout)
+        messages_database_layout.addWidget(messages_database_file_label)
+        messages_database_layout.addWidget(self.messages_database_file_input)
+        #messages_database_layout.addWidget(self.messages_database_file_button)
+
+        database_files_layout.addLayout(users_database_layout)
+        database_files_layout.addLayout(messages_database_layout)
+
+        databases_box.setLayout(database_files_layout)
 
         server_status_layout.addWidget(server_status_label)
         server_status_layout.addWidget(self.server_status_state)
@@ -94,11 +117,15 @@ class MainUi(QWidget):
         server_control_box_layout.addLayout(server_uptime_layout)
         server_control_box_layout.addLayout(server_buttons_layout)
 
+        server_folder_layout.addStretch()
+        server_folder_layout.addWidget(server_folder_button)
+
         server_control_box.setLayout(server_control_box_layout)
 
         layout.addWidget(ssl_box)
-        layout.addWidget(database_box)
+        layout.addWidget(databases_box)
         layout.addWidget(server_control_box)
+        layout.addLayout(server_folder_layout)
 
         self.fill_inputs(self.files)
 
@@ -111,16 +138,19 @@ class MainUi(QWidget):
                 self.key_file_input.setText(file_path)
 
             elif file_path.endswith(".db"):
-                self.database_file_input.setText(file_path)
+                self.users_database_file_input.setText(file_path)
 
     def send_file_path(self, file_type):
-        if file_type == ".crt":
+        if file_type == "certificate":
             file_path, _ = QFileDialog.getOpenFileName(self, "Select file", "", "Certificate files (*.crt)")
 
-        elif file_type == ".key":
+        elif file_type == "key":
             file_path, _ = QFileDialog.getOpenFileName(self, "Select file", "", "Key files (*.key)")
 
-        elif file_type == ".db":
+        elif file_type == "users_database":
+            file_path, _ = QFileDialog.getOpenFileName(self, "Select file", "", "Database files (*.db)")
+
+        elif file_type == "messages_database":
             file_path, _ = QFileDialog.getOpenFileName(self, "Select file", "", "Database files (*.db)")
 
         if not file_path:
@@ -150,9 +180,9 @@ class MainUi(QWidget):
         self.certificate_file_input.setEnabled(False)
         self.key_file_input.setEnabled(False)
         self.database_file_input.setEnabled(False)
-        self.key_file_button.setEnabled(False)
-        self.database_file_button.setEnabled(False)
-        self.certificate_file_button.setEnabled(False)
+        #self.key_file_button.setEnabled(False)
+        #self.database_file_button.setEnabled(False)
+        #self.certificate_file_button.setEnabled(False)
         self.tray.set_server_status("Running")
 
     def stop_server(self):
@@ -164,9 +194,9 @@ class MainUi(QWidget):
         self.certificate_file_input.setEnabled(True)
         self.key_file_input.setEnabled(True)
         self.database_file_input.setEnabled(True)
-        self.key_file_button.setEnabled(True)
-        self.database_file_button.setEnabled(True)
-        self.certificate_file_button.setEnabled(True)
+        #self.key_file_button.setEnabled(True)
+        #self.database_file_button.setEnabled(True)
+        #self.certificate_file_button.setEnabled(True)
         self.tray.set_server_status("Stopped")
 
         self.update_timer(0, 0, 0)
